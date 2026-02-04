21a476a0ec16011d84b8c7abd3dfeb6afd2dbba2
AlwinNg <alwinng420@gmail.com>
2026-02-04 18:45:47 -0500

    interpolation
    Co-authored-by: Mohammad Rahman <mohammadr09@users.noreply.github.com>

---
+++ src/main/java/com/stuypulse/robot/util/SpindexerInterpolation.java
@@ -0,0 +1,23 @@
+package com.stuypulse.robot.util;
+
+import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
+
+public class SpindexerInterpolation {
+    private static final InterpolatingDoubleTreeMap interpolatingDoubleTreeMap;
+
+    private static final double[][] voltageAndDistance = {
+        // { voltage, distance }
+        {1,1}
+    };
+
+    static {
+        interpolatingDoubleTreeMap = new InterpolatingDoubleTreeMap();
+        for (double[] data: voltageAndDistance) {
+            interpolatingDoubleTreeMap.put(data[1], data[0]);
+        }
+    }
+
+    public static double getVoltage(double distanceInMeters){
+        return interpolatingDoubleTreeMap.get(distanceInMeters);
+    }
+}
